6611a8ccf5e51a1f00a4374194855f920c5ae4bb
Tolga <109863384+TolgaYuceel@users.noreply.github.com>
2023-12-05 22:23:50 +0300

    uploadthing setup

---
+++ components/file-upload.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+import "@uploadthing/react/styles.css"
+import { X } from "lucide-react";
+import Image from "next/image";
+
+interface FileUploadProps {
+    onChange: (url?: string) => void;
+    value: string;
+    endpoint: "messageFile" | "serverImage"
+}
+
+export const FileUpload = ({ onChange, value, endpoint} : FileUploadProps) => {
+
+    const fileType = value?.split(".").pop();
+    if(value && fileType !== "pdf") {
+        return(
+            <div className="relative h-32 w-32">
+                <Image
+                    fill
+                    src={value}
+                    alt="Upload"
+                    className="rounded-full object-cover"
+                />
+                <button
+                    onClick={() => onChange("")}
+                    className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+                    type="button"
+                >
+                    <X className="h-6 w-6"/>
+                </button>
+            </div>
+        )
+    }
+
+    return(
+        <UploadDropzone
+            endpoint={endpoint}
+            onClientUploadComplete={(res) => {
+                onChange(res?.[0].url);
+            }}
+            onUploadError={(error: Error) => {
+                console.log(error);
+            }}
+            className="cursor-pointer"
+        />
+    )
+} 
