4678c40577c9eee5df3e08247306abbd9704ebf7
Tolga <109863384+TolgaYuceel@users.noreply.github.com>
2023-12-09 22:19:32 +0300

    manage members component has been created and improvments on some components

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -1,6 +1,5 @@
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { Nerko_One } from "next/font/google";
 import { NextResponse } from "next/server";
 
 export async function PATCH(
--- components/modals/edit-server-modal.tsx
+++ components/modals/edit-server-modal.tsx
@@ -70,10 +70,10 @@ export const EditServerModal = () => {
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
                 <DialogHeader className="pt-8 px-6">
                     <DialogTitle className="text-2xl text-center font-bold">
-                        Customize Your Server
+                        Edit Your Server
                     </DialogTitle>
                     <DialogDescription className="text-center text-zinc-500">
-                        Give your server a personality with a name and an image. You can always change it later.
+                        You can organize your server any way you want, but be careful not to ignore our community guidelines.
                     </DialogDescription>
                 </DialogHeader>
                 <Form {...form}>
