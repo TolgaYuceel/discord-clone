9c2e223910efbc2975d84e778fca96547912f114
Tolga <109863384+TolgaYuceel@users.noreply.github.com>
2023-12-10 21:57:40 +0300

    leave and delete server modals developed

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         primary: "bg-indigo-500 text-white hover:bg-indigo-500/90",
+        danger: "bg-rose-600 text-white hover:bg-rose-500 transition-all",
       },
       size: {
         default: "h-10 px-4 py-2",
