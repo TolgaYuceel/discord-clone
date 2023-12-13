4fe27a5dff77bb67dc1ee96ef76ac461da1151cb
Tolga <109863384+TolgaYuceel@users.noreply.github.com>
2023-12-13 18:33:17 +0300

    Modals for editing and deleting channels have been developed.

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -6,6 +6,7 @@ import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
 import { cn } from "@/lib/utils";
 import { ActionTooltip } from "@/components/action-tooltip";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerChannelProps {
     channel: Channel;
@@ -20,6 +21,7 @@ const iconMap = {
 }
 
 export const ServerChannel = ({ channel, server, role } : ServerChannelProps) => {
+    const { onOpen } = useModal();
     const params = useParams();
     const router = useRouter();
 
@@ -45,11 +47,13 @@ export const ServerChannel = ({ channel, server, role } : ServerChannelProps) =>
                 <div className="ml-auto flex items-center gap-x-2">
                     <ActionTooltip label="Edit">
                         <Edit
+                            onClick={() => onOpen("editChannel", { server, channel})}
                             className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
                         />
                     </ActionTooltip>
                     <ActionTooltip label="Delete">
                         <Trash
+                            onClick={() => onOpen("deleteChannel", { server, channel })}
                             className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
                         />
                     </ActionTooltip>
